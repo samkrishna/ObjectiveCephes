32abe59083a6231cbb5e6f7b9c83978fb4c88f6b
Sam Krishna <sam.krishna@gmail.com>
2021-11-16 08:36:43 -0800

    Deprecate dead setup/teardown test functions

--- ObjectiveCephesTests/ObjectiveCephesTests.m
+++ ObjectiveCephesTests/ObjectiveCephesTests.m
@@ -15,16 +15,6 @@
 
 @implementation ObjectiveCephesTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testBesselCases {
     // This is modified from the bessel.t code from the Math-Cephes Perl Module
 
