80835f4931a3c3de7181161fb9459c318a4587cb
Sam Krishna <sam.krishna@gmail.com>
2021-11-17 15:44:01 -0800

    Fill out -testMiscCases with tests

--- ObjectiveCephesTests/ObjectiveCephesTests.m
+++ ObjectiveCephesTests/ObjectiveCephesTests.m
@@ -477,52 +477,43 @@
     XCTAssertEqualWithAccuracy(cfs_zeta(x, n), 0.2729056157, 0.00000001);
     XCTAssertEqualWithAccuracy(cfs_dawsn(x), 0.2645107600, 0.00000001);
 
-//    my ($flagf, $S, $C) = fresnl($x);
-//    ok( $flagf, 0);
-//    ok( $S, .4557046121);
-//    ok( $C, .6362860449);
-//    my ($flagt, $Si, $Ci) = sici($x);
-//    ok( $flagt, 0);
-//    ok( $Si, 1.687624827);
-//    ok( $Ci, .3750745990);
-//    my ($flagh, $Shi, $Chi) = shichi($x);
-//    ok( $flagh, 0);
-//    ok( $Shi, 2.884902918);
-//    ok( $Chi, 2.847711781);
-//    ok( expn($n, $x), .02352065665);
-//    ok( ei($x), 5.732614700);
-//    ok( spence($x), -.9574053086);
-//    my ($flaga, $ai, $aiprime, $bi, $biprime) = airy($x);
-//    ok( $flaga, 0);
-//    ok( $ai, .02561040442);
-//    ok( $aiprime, -.04049726324);
-//    ok( $bi, 4.267036582);
-//    ok( $biprime, 5.681541770);
-//    ok( erf($x), .9981371537);
-//    ok( erfc($x), .001862846298);
-//    ok( struve($n, $x), .1186957024);
-//    my $r = plancki(0.1, 200);
-//    ok( $r, 90.72805158);
-//    $r = simpson(\&fun, 0, 100, 1e-8, 1e-06, 100);
-//    ok( $r, sin(100));
-//    my ($num, $den) = bernum(16);
-//    ok( $num, -3617);
-//    ok( $den, 510);
-//    ($num, $den) = bernum();
-//    ok( $num->[26], 8553103);
-//    ok( $den->[26], 6);
-//    ok( polylog(3, 0.2), 0.2053241957);
-//    ok( polylog(7, 1), 1.008349277);
-//    my $v1 = [1, 2, -1];
-//    my $v2 = [2, -1, 3];
-//    my $c = -3 / sqrt(6) / sqrt(14);
-//    ok( vecang($v1, $v2), acos($c));
-//
-//    sub fun {
-//      my $x = shift;
-//      return cos($x);
-//    }
+    double flagf, S, C;
+    flagf = cfs_fresnl(x, &S, &C);
+    XCTAssertTrue(flagf == 0);
+    XCTAssertEqualWithAccuracy(S, 0.4557046121, 0.000001);
+    XCTAssertEqualWithAccuracy(C, 0.6362860449, 0.000001);
 
+    double flagt, Si, Ci;
+    flagt = cfs_sici(x, &Si, &Ci);
+    XCTAssertTrue(flagt == 0);
+    XCTAssertEqualWithAccuracy(Si, 1.687624827, 0.000001);
+    XCTAssertEqualWithAccuracy(Ci, 0.3750745990, 0.000001);
+
+    double flagh, Shi, Chi;
+    flagh = cfs_shichi(x, &Shi, &Chi);
+    XCTAssertTrue(flagh == 0);
+    XCTAssertEqualWithAccuracy(Shi, 2.884902918, 0.00000001);
+    XCTAssertEqualWithAccuracy(Chi, 2.847711781, 0.00000001);
+    XCTAssertEqualWithAccuracy(cfs_expn(n, x), 0.02352065665, 0.00000001);
+    XCTAssertEqualWithAccuracy(cfs_ei(x), 5.732614700, 0.00000001);
+    XCTAssertEqualWithAccuracy(cfs_spence(x), -0.9574053086, 0.00000001);
+
+    double flaga, ai, aiprime, bi, biprime;
+    flaga = cfs_airy(x, &ai, &aiprime, &bi, &biprime);
+    XCTAssertTrue(flaga == 0);
+    XCTAssertEqualWithAccuracy(ai, 0.02561040442, 0.00000001);
+    XCTAssertEqualWithAccuracy(aiprime, -.04049726324, 0.00000001);
+    XCTAssertEqualWithAccuracy(bi, 4.267036582, 0.00000001);
+    XCTAssertEqualWithAccuracy(biprime, 5.681541770, 0.00000001);
+    XCTAssertEqualWithAccuracy(cfs_erf(x), .9981371537, 0.00000001);
+    XCTAssertEqualWithAccuracy(cfs_erfc(x), .001862846298, 0.00000001);
+    XCTAssertEqualWithAccuracy(cfs_struve(n, x), .1186957024, 0.00000001);
+
+    double r = cfs_plancki(0.1, 200);
+    XCTAssertEqualWithAccuracy(r, 90.72805158, 0.00000001);
+
+    XCTAssertEqualWithAccuracy(cfs_polylog(3, 0.2), 0.2053241957, 0.00000001);
+    XCTAssertEqualWithAccuracy(cfs_polylog(7, 1), 1.008349277, 0.00000001);
 }
 
 - (void)testPolynomialCases
