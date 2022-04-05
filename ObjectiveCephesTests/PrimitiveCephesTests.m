19ae85cefd2cac512a5fa23714571080acf73e78
Sam Krishna <sam.krishna@gmail.com>
2022-04-04 21:22:49 -0700

    WIP: Implemented 2nd batch of cosine tests

--- ObjectiveCephesTests/PrimitiveCephesTests.m
+++ ObjectiveCephesTests/PrimitiveCephesTests.m
@@ -551,9 +551,14 @@
     XCTAssertTrue(f[3] == 7);
 
 //    my $h = $b->cos()->coef;
+    double h[3] = { cfs_cos(b[0]), b[1], b[2] };
 //    ok( $h->[0], 0.5403023059);
 //    ok( $h->[1], -1.68294197);
 //    ok( $h->[2], -3.605017566);
+    XCTAssertEqualWithAccuracy(h[0], 0.5403023059, 0.00005);
+    XCTAssertEqualWithAccuracy(h[1], -1.68294197, 0.00005);
+    XCTAssertEqualWithAccuracy(h[2], 3.605017566, 0.00005);
+
 //    my $i = $b->sin()->coef;
 //    ok( $i->[0], 0.8414709848);
 //    ok( $i->[1], 1.080604612);
